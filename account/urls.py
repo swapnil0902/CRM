0a7f49cbe90092ce28b1dd2e299aa949d51dbe88
Sivateja <sivatejat509@gmail.com>
2024-08-21 09:51:38 +0530

    2nd day second commit

--- account/urls.py
+++ account/urls.py
@@ -5,8 +5,10 @@ from . import views
 urlpatterns = [
     path('test1/', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('signup/', views.signup, name='signup'),
     path('groups/', views.group_list, name='group_list'),
     path('create/', views.group_create, name='group_create'),
     path('groups/<int:pk>/update/', views.group_update, name='group_update'),
     path('groups/<int:pk>/delete/', views.group_delete, name='group_delete'),
+    path('activate/', views.activate_password, name='activate_password'),
 ]
