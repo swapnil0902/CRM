b5b5a21a110c2cc91873494d962dc2d4b9c729ee
swapnil0902 <swadutt5@gmail.com>
2024-08-23 14:14:08 +0530

    4th day Swapnil 2

--- appointment/views.py
+++ appointment/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view
 from .models import Appointment
 from .forms import AppointmentForm
 
@@ -44,3 +45,31 @@ def appointment_delete(request, pk):
 def appointment_list(request):
     appointments = Appointment.objects.filter(attendees=request.user)
     return render(request, 'appointment/appointment_list.html', {'appointments': appointments})
+
+
+@login_required
+def company_appointment_list(request):
+    company = request.user.userprofile.company
+    appointments = Appointment.objects.filter(company=company)
+    return render(request, 'appointment/company_appointment_list.html', {'appointments': appointments})
+
+@login_required
+def company_appointment_update(request, pk):
+    company = request.user.userprofile.company
+    appointment = get_object_or_404(Appointment, pk=pk, company=company)
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST, instance=appointment)
+        if form.is_valid():
+            form.save()
+            return redirect('company_appointment_list')
+    else:
+        form = AppointmentForm(instance=appointment)
+    return render(request, 'appointment/company_appointment_form.html', {'form': form})
+
+
+@login_required
+@api_view(['GET', 'DELETE'])
+def company_appointment_delete(request, pk):
+    appointment = get_object_or_404(Appointment, pk=pk)
+    appointment.delete()
+    return redirect('company_appointment_list')
