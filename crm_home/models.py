5e7db728296fff64c740309f67fafc2032d3682d
Sivateja <sivatejat509@gmail.com>
2024-08-26 12:24:32 +0530

    5th day sivateja commit 3

--- crm_home/models.py
+++ crm_home/models.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-# Create your models here.
 
 class Company(models.Model):
     id = models.AutoField(primary_key=True)
