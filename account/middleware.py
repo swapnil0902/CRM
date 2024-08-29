65ce28b1b31c661f00cace9fdc49b66b45bdeb8e
Sivateja <sivatejat509@gmail.com>
2024-08-29 16:51:14 +0530

    8th day sivateja last commit

---
+++ account/middleware.py
@@ -0,0 +1,15 @@
+# myapp/middleware.py
+from django.conf import settings
+from django.utils import timezone
+
+class SessionTimeoutMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Extend session expiry time on user interaction
+        if request.user.is_authenticated:
+            request.session.set_expiry(settings.SESSION_COOKIE_AGE)
+        
+        response = self.get_response(request)
+        return response
