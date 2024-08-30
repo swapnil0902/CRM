6d3717afaf608a5773a974450e01d8d28504dbe3
Sivateja <sivatejat509@gmail.com>
2024-08-30 11:21:03 +0530

    9th day Sivateja Commit -2

--- appointment/forms.py
+++ appointment/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Appointment
 from django.shortcuts import get_object_or_404
 from crm_home.models import UserProfile
-from customer.models import Customer
+from customer.models import Customer,User
 
 ##################################          #########################################################
 class AppointmentForm(forms.ModelForm):
@@ -13,22 +13,22 @@ class AppointmentForm(forms.ModelForm):
         model = Appointment
         fields = ['title', 'description', 'start_date', 'end_date', 'location', 'attendees', 'customer']
 
-        
-
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
         super(AppointmentForm, self).__init__(*args, **kwargs)
-        
-        if user and user.groups.filter(name='Staff').exists():
-            self.fields.pop('attendees')
 
-        try:
-            user_profile = UserProfile.objects.get(staff=user)
-            self.fields['customer'].queryset = Customer.objects.filter(company=user_profile.company)
-        except UserProfile.DoesNotExist:
-            # Handle the case where UserProfile does not exist
-            self.fields['customer'].queryset = Customer.objects.none()
+        if user:
+            try:
+                user_profile = UserProfile.objects.get(staff=user)
+                self.fields['customer'].queryset = Customer.objects.filter(company=user_profile.company)
+                self.fields['attendees'].queryset = User.objects.filter(userprofile__company=user_profile.company)
 
+                if user.groups.filter(name='Staff').exists():
+                    self.fields.pop('attendees')
+
+            except UserProfile.DoesNotExist:
+                self.fields['customer'].queryset = Customer.objects.none()
+                self.fields['attendees'].queryset = User.objects.none()
 
 ##################################          #########################################################
 
